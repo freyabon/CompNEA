1c8a7f09e2b7f93b1b49b3c37744a7858a977544
freyabon <freyabartley@yahoo.com>
2023-11-19 17:01:40 +0000

    Update app

---
+++ sustainableStockApp/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sustainableStockApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            HttpContext.Session.SetString("UserId", "123"); //change this to query database
+            return RedirectToAction("Dashboard", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("UserId");
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ sustainableStockApp/wwwroot/js/site.js
@@ -0,0 +1,52 @@
+﻿console.log('Loaded');
+
+$(document).ready(function () {
+
+    $("#blockRegister").hide();
+
+    $("#toggleSignIn").click(function () {
+        $("#blockSignIn").show();
+        $("#blockRegister").hide();
+        console.log('Sign in Clicked');
+    });
+
+    $("#toggleRegister").click(function () {
+        $("#blockSignIn").hide();
+        $("#blockRegister").show();
+        console.log('Register Clicked');
+    });
+
+    $('#btnSubmit').on('click', function (e) {
+        /*e.preventDefault();*/
+        console.log('clicked');
+        //$('#login_page').toggle();
+        username = $('#inpUser')[0].value;
+        password = $('#inpPass')[0].value;
+        console.log(username);
+        console.log(password);
+
+        /*$("#outUsername").html(username);*/
+    })
+
+
+
+    $('#btnRegister').on('click', function (e) {
+        username = $('#regUser')[0].value;
+        password = $('#regPass')[0].value;
+        password2 = $('#regPass2')[0].value;
+        console.log(username);
+        console.log(password);
+
+        if (password === password2) {
+            $("#spPassError").hide();
+            console.log('Registered');
+            usernames.push(username);
+            passwords.push(password);
+        } else {
+            e.preventDefault();
+            $("#spPassError").show();
+        }
+    })
+});
+
+
