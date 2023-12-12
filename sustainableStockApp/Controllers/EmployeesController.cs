3d0187abaf88c9b377446e2e91fdd1ae6deb485d
freyabon <freyabartley@yahoo.com>
2023-12-12 12:30:13 +0000

    Update

--- sustainableStockApp/Controllers/EmployeesController.cs
+++ sustainableStockApp/Controllers/EmployeesController.cs
@@ -1,15 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using sustainableStockApp.Data;
+using sustainableStockApp.Models;
+using sustainableStockApp.Models.Domain;
+
 
 namespace sustainableStockApp.Controllers
 {
 	public class EmployeesController : Controller
 	{
-		[HttpGet]
+		private readonly MVCDemoDBContext mvcDemoDbContext;
+        public EmployeesController(MVCDemoDBContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+        [HttpGet]
 		public IActionResult Add()
 		{
 			return View();
 		}
 
+		[HttpPost]
+		public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+		{
+			var employee = new Employee()
+			{
+				Id = Guid.NewGuid(),
+				Name = addEmployeeRequest.Name,
+				Email = addEmployeeRequest.Email,
+				Salary = addEmployeeRequest.Salary,
+				Department = addEmployeeRequest.Department,
+				DateOfBirth = addEmployeeRequest.DateOfBirth,
+			};
+
+			await mvcDemoDbContext.Employees.AddAsync(employee);
+			await mvcDemoDbContext.SaveChangesAsync();
+			return RedirectToAction("Add");
+		}
 		
 	}
 }
